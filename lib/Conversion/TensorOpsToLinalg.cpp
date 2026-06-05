87bed002a22cc92e72b5005659b940f795985ae4
xyf2002 <2316639449@qq.com>
2026-06-06 00:19:23 +0100

    fix: add init tensors to linalg lowering patterns

--- lib/Conversion/TensorOpsToLinalg.cpp
+++ lib/Conversion/TensorOpsToLinalg.cpp
@@ -8,8 +8,8 @@
 #include "TensorOps/TensorOpsPasses.h"
 
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
-#include "mlir/Dialect/Linalg/Transforms/Transforms.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Transforms/DialectConversion.h"
@@ -18,60 +18,63 @@
 
 using namespace mlir;
 
+/// Create an empty tensor (init) of the given type.
+static Value createEmptyTensor(PatternRewriter &rewriter, Location loc,
+                                RankedTensorType type) {
+  SmallVector<Value> dynSizes;
+  for (int64_t i = 0; i < type.getRank(); ++i) {
+    if (type.isDynamicDim(i))
+      dynSizes.push_back(rewriter.create<tensor::DimOp>(loc, Value(), i));
+  }
+  return rewriter.create<tensor::EmptyOp>(loc, type.getShape(),
+                                          type.getElementType(), dynSizes);
+}
+
 namespace {
 
-//===----------------------------------------------------------------------===//
-// MatmulOp -> linalg.matmul
-//===----------------------------------------------------------------------===//
-
-struct MatmulOpLowering : public OpRewritePattern<tensor::MatmulOp> {
+struct MatmulOpLowering : public OpRewritePattern<ten::MatmulOp> {
   using OpRewritePattern::OpRewritePattern;
 
-  LogicalResult matchAndRewrite(tensor::MatmulOp op,
+  LogicalResult matchAndRewrite(ten::MatmulOp op,
                                 PatternRewriter &rewriter) const override {
-    auto lhsType = op.getLhs().getType();
-    auto rhsType = op.getRhs().getType();
-    auto resultType = op.getResult().getType();
-
-    rewriter.replaceOpWithNewOp<linalg::MatmulOp>(
-        op, resultType, ValueRange{op.getLhs(), op.getRhs()},
-        ValueRange{});
-    return success();
-  }
-};
-
-//===----------------------------------------------------------------------===//
-// AddOp -> linalg.elemwise_broadcast (generic add)
-//===----------------------------------------------------------------------===//
-
-struct AddOpLowering : public OpRewritePattern<tensor::AddOp> {
-  using OpRewritePattern::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(tensor::AddOp op,
-                                PatternRewriter &rewriter) const override {
-    auto resultType = op.getResult().getType();
-    rewriter.replaceOpWithNewOp<linalg::AddOp>(
-        op, resultType, ValueRange{op.getLhs(), op.getRhs()},
-        ValueRange{});
-    return success();
-  }
-};
-
-//===----------------------------------------------------------------------===//
-// ReluOp -> linalg.generic { max(0, x) }
-//===----------------------------------------------------------------------===//
-
-struct ReluOpLowering : public OpRewritePattern<tensor::ReluOp> {
-  using OpRewritePattern::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(tensor::ReluOp op,
-                                PatternRewriter &rewriter) const override {
-    auto input = op.getInput();
-    auto resultType = op.getResult().getType();
-
-    // Build linalg.generic for element-wise max(0, x)
     auto loc = op.getLoc();
-    auto inputType = input.getType().cast<RankedTensorType>();
+    auto resultType = mlir::cast<RankedTensorType>(op.getResult().getType());
+    Value init = rewriter.create<tensor::EmptyOp>(loc, resultType.getShape(),
+                                                   resultType.getElementType());
+    rewriter.replaceOpWithNewOp<linalg::MatmulOp>(
+        op, resultType, ValueRange{op.getLhs(), op.getRhs()}, init);
+    return success();
+  }
+};
+
+struct AddOpLowering : public OpRewritePattern<ten::AddOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ten::AddOp op,
+                                PatternRewriter &rewriter) const override {
+    auto loc = op.getLoc();
+    auto resultType = mlir::cast<RankedTensorType>(op.getResult().getType());
+    Value init = rewriter.create<tensor::EmptyOp>(loc, resultType.getShape(),
+                                                   resultType.getElementType());
+    rewriter.replaceOpWithNewOp<linalg::AddOp>(
+        op, resultType, ValueRange{op.getLhs(), op.getRhs()}, init);
+    return success();
+  }
+};
+
+struct ReluOpLowering : public OpRewritePattern<ten::ReluOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ten::ReluOp op,
+                                PatternRewriter &rewriter) const override {
+    auto loc = op.getLoc();
+    auto input = op.getInput();
+    auto resultType = mlir::cast<RankedTensorType>(op.getResult().getType());
+
+    Value init = rewriter.create<tensor::EmptyOp>(loc, resultType.getShape(),
+                                                   resultType.getElementType());
+    auto inputType = mlir::cast<RankedTensorType>(input.getType());
+
     SmallVector<AffineMap> indexMaps = {
         AffineMap::getMultiDimIdentityMap(inputType.getRank(), rewriter.getContext()),
         AffineMap::getMultiDimIdentityMap(inputType.getRank(), rewriter.getContext())};
@@ -79,7 +82,7 @@ struct ReluOpLowering : public OpRewritePattern<tensor::ReluOp> {
         inputType.getRank(), utils::IteratorType::parallel);
 
     auto genericOp = rewriter.create<linalg::GenericOp>(
-        loc, resultType, input, ValueRange{}, indexMaps, iteratorTypes,
+        loc, resultType, ValueRange{input}, ValueRange{init}, indexMaps, iteratorTypes,
         [](OpBuilder &b, Location loc, ValueRange args) {
           Value zero = b.create<arith::ConstantOp>(loc, b.getF32FloatAttr(0.0f));
           Value cmp = b.create<arith::CmpFOp>(loc, arith::CmpFPredicate::OGT,
@@ -93,10 +96,6 @@ struct ReluOpLowering : public OpRewritePattern<tensor::ReluOp> {
   }
 };
 
-//===----------------------------------------------------------------------===//
-// TensorOpsToLinalgPass
-//===----------------------------------------------------------------------===//
-
 struct TensorOpsToLinalgPass
     : public PassWrapper<TensorOpsToLinalgPass, OperationPass<ModuleOp>> {
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(TensorOpsToLinalgPass)
@@ -107,13 +106,17 @@ struct TensorOpsToLinalgPass
   }
 
   void runOnOperation() override {
-    auto *ctx = &getContext();
+    MLIRContext *ctx = &getContext();
     ConversionTarget target(*ctx);
+
+    ctx->loadDialect<arith::ArithDialect, linalg::LinalgDialect,
+                     tensor::TensorDialect, func::FuncDialect>();
 
     target.addLegalDialect<arith::ArithDialect,
                           linalg::LinalgDialect,
-                          tensor::TensorDialect>();
-    target.addIllegalDialect<tensor::TensorOpsDialect>();
+                          tensor::TensorDialect,
+                          func::FuncDialect>();
+    target.addIllegalDialect<ten::TensorOpsDialect>();
 
     RewritePatternSet patterns(ctx);
     patterns.add<MatmulOpLowering, AddOpLowering, ReluOpLowering>(ctx);
@@ -127,6 +130,6 @@ struct TensorOpsToLinalgPass
 
 } // namespace
 
-std::unique_ptr<mlir::Pass> tensor::createLowerToLinalgPass() {
+std::unique_ptr<mlir::Pass> ten::createLowerToLinalgPass() {
   return std::make_unique<TensorOpsToLinalgPass>();
 }
